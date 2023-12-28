230a1d19194298a1cf3290d9089c121c43276ff1
koushiksamanta23 <koushiksamanta23@hotmail.com>
2023-12-28 17:07:21 +0530

    update

--- CSharpLearning/Beginner.cs
+++ CSharpLearning/Beginner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,18 @@ namespace CSharpLearning
             Console.WriteLine($"Sum of 2 numbers {a} and {b} is {(a + b)}");
             Console.WriteLine("Sum of 2 numbers {0} and {1} is {2}", a, b, (a + b));
         }
+
+        public void add(int a, int b)
+        {
+            Console.WriteLine("add number:");
+            Console.WriteLine($"enter first number: {a}");
+            Console.WriteLine($"Enter Second Number: {b}");
+
+            Console.WriteLine($"Sum of 2 numbers {a} and {b} is {(a + b)}");
+            Console.WriteLine("Sum of 2 numbers {0} and {1} is {2}", a, b, (a + b));
+        }
+
+
         public void substract()
         {
             Console.WriteLine("substract number:");
@@ -32,6 +45,16 @@ namespace CSharpLearning
             Console.WriteLine("enter second number:");
             var b = int.Parse(Console.ReadLine());
             Console.WriteLine("substract of 2 number {0} and {1} is {2}", a, b, (a - b));
+        }
+
+        public void substract(int a, int b)
+        {
+            Console.WriteLine("substract number:");
+            Console.WriteLine($"enter first number: {a}");
+            Console.WriteLine($"Enter Second Number: {b}");
+
+            Console.WriteLine($"Sum of 2 numbers {a} and {b} is {(a - b)}");
+            Console.WriteLine("Sum of 2 numbers {0} and {1} is {2}", a, b, (a - b));
         }
         public void multiply()
         {
@@ -42,6 +65,15 @@ namespace CSharpLearning
             var b = int.Parse(Console.ReadLine());
             Console.WriteLine("multiply of 2 number {0} and {1} is {2}", a, b, (a * b));
         }
+        public void multiply(int a, int b)
+        {
+            Console.WriteLine("multiply number:");
+            Console.WriteLine($"enter first number: {a}");
+            Console.WriteLine($"Enter Second Number: {b}");
+
+            Console.WriteLine($"multiplication of 2 numbers {a} and {b} is {(a * b)}");
+            Console.WriteLine("multiplication of 2 numbers {0} and {1} is {2}", a, b, (a * b));
+        }
         public void divide()
         {
             Console.WriteLine("divide number:");
@@ -50,6 +82,63 @@ namespace CSharpLearning
             Console.WriteLine("enter second number:");
             var b = double.Parse(Console.ReadLine());
             Console.WriteLine("division of 2 number {0} and {1} : quotient {2} and remiender {3}", a, b, (a / b), (a % b));
+            
+        }
+        public void divide(double a, double b)
+        {
+            Console.WriteLine("divide number:");
+            Console.WriteLine($"enter first number: {a}");
+            Console.WriteLine($"Enter Second Number: {b}");
+
+            Console.WriteLine($"divide of 2 numbers {a} and {b} is {(a / b)}");
+            Console.WriteLine("divide of 2 numbers {0} and {1} is {2}", a, b, (a / b));
+        }
+
+        public void runAll()
+        {
+
+            Console.WriteLine("Getting values for addition.\nEnter first No:");
+            var add1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second No:");
+            var add2 = int.Parse(Console.ReadLine());
+            add(add1, add2);
+
+            Console.WriteLine("Getting value for substract.\nEnter first no:");
+            var sub1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second no:");
+            var sub2 = int.Parse(Console.ReadLine());
+            substract(sub1, sub2);
+
+            Console.WriteLine("Getting value for multiply.\nEnter first no:");
+            var multi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second no:");
+            var multi2 = int.Parse(Console.ReadLine());
+            multiply(multi1, multi2);
+
+            Console.WriteLine("Getting value for divide.\nEnter first no:");
+            var div1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            var div2 = int.Parse(Console.ReadLine());
+            divide(div1, div2);
+
+            Console.WriteLine("Getting value for operation all.\nEnter first no:");
+            var all1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            var all2 = int.Parse(Console.ReadLine());
+            runAll(all1, all2);
+
+        }
+        public void runAll(int no1, int no2)
+        {
+           Console.WriteLine("performing addition");
+            add(no1,no2);
+            Console.WriteLine("performing substract");
+            substract(no1, no2);
+            Console.WriteLine("performing multiply");
+            multiply(no1, no2);
+            Console.WriteLine("performing divide");
+            divide(no1, no2);
+
         }
     }
 }
--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -2,5 +2,22 @@
 using CSharpLearning;
 
 var beginner = new Beginner();
+//Console.WriteLine(enter first number);
+
 //beginner.add();
+/*
+beginner.add();
+beginner.substract();
+beginner.multiply();
 beginner.divide();
+*/
+beginner.runAll();
+
+/*
+Console.WriteLine("Getting values for addition.\nEnter first No:");
+var add1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second No:");
+var add2 =  int.Parse(Console.ReadLine());
+
+beginner.add(add1, add2);
+*/
